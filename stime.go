3f8ec0f1e5b41a965eadb5eca23258d9292e1c0f
Iain Munro <iain17@me.com>
2018-04-11 10:37:27 +0200

    use a fallback

--- stime.go
+++ stime.go
@@ -3,16 +3,28 @@ package stime
 import (
 	"github.com/beevik/ntp"
 	"time"
+	"github.com/iain17/logger"
 )
 
 var response = ntpResponse()
 
+var servers = []string{
+	"time.google.com",
+	"pool.ntp.org",
+	"0.pool.ntp.org",
+	"1.pool.ntp.org",
+}
+
 func ntpResponse() ntp.Response {
-	response, err := ntp.Query("time.google.com")
-	if err != nil {
-		return ntp.Response{}
+	for _, server := range servers {
+		response, err := ntp.Query(server)
+		if err != nil {
+			logger.Warning(err)
+			continue
+		}
+		return *response
 	}
-	return *response
+	return ntp.Response{}
 }
 
 func Now() time.Time {
